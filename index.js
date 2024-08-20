6187d66f6c360d05e194d01def25c0744cab9703
Kuldeep Jambhulkar <kuldeep.jambhulkar@compusoftgroup.com>
2024-08-20 17:29:09 +0530

    Make the countdown stop when reach target date.

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ function formatNumber(number) {
 function updateCountdown() {
     const now = new Date();
     const targetDate = new Date(now.getFullYear(), 7, 23, 17, 0, 0, 0); // August is month 7 (0-based index)
-    if (now > targetDate) {
-        targetDate.setFullYear(now.getFullYear() + 1);
-    }
+    // if (now > targetDate) {
+    //     targetDate.setFullYear(now.getFullYear() + 1);
+    // }
     const timeDifference = targetDate - now;
 
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-    const milliseconds = Math.floor(timeDifference % 1000 / 10); // Convert milliseconds to two-digit format
+    let days, hours, minutes, seconds, milliseconds;
+
+    if (timeDifference <= 0) {
+        days = hours = minutes = seconds = milliseconds = 0;
+        const allNumbers = document.querySelectorAll('.number');
+        allNumbers.forEach(ele => ele.classList.add('blink'));
+    } else {
+        days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        milliseconds = Math.floor(timeDifference % 1000 / 10); // Convert milliseconds to two-digit format
+    }
 
     document.getElementById('days').querySelector('.number').textContent = formatNumber(days);
     document.getElementById('hours').querySelector('.number').textContent = formatNumber(hours);
@@ -22,7 +30,9 @@ function updateCountdown() {
     document.getElementById('seconds').querySelector('.number').textContent = formatNumber(seconds);
     document.getElementById('milliseconds').querySelector('.number').textContent = formatNumber(milliseconds);
 
-    setTimeout(updateCountdown, 1);
+    if (timeDifference > 0) {
+        setTimeout(updateCountdown, 1);
+    }
 }
 
 updateCountdown();
